d4e96f0b3978ae7b265c8be0cf2aa12e9db93986
tgorup <tom@rookconsulting.com>
2014-09-10 22:17:19 -0400

    Increased logging and exception handling.

--- mods/mod_global.py
+++ mods/mod_global.py
@@ -1,11 +1,42 @@
 #!/usr/bin/python
 
+import logging
+import os
+
+current_directory = os.getcwd()
+logger = logging.getLogger('ctfCollector')
+hdlr = logging.FileHandler(current_directory + '/log/setup.log')
+formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
+hdlr.setFormatter(formatter)
+logger.addHandler(hdlr)
+logger.setLevel(logging.INFO)
+
 def insert_new_user(username):
 
     import sqlite3
-    conn = sqlite3.connect('database/ctfCollector.db')    # Setup connection to sqlite database
-    c = conn.cursor()
-    # Insert into user_flags table the username and flag they have obtained
-    c.execute('''INSERT INTO user_points VALUES (?,?)''', (username, '0'))
-    conn.commit()    # commit the changes to the database
-    conn.close()    # Close connection to sqlite database
+
+    try:
+        conn = sqlite3.connect('database/ctfCollector.db')    # Setup connection to sqlite database
+    except Exception, e:
+        logger.inf(e)
+
+    try:
+        c = conn.cursor()
+    except Exception, e:
+        logger.inf(e)
+
+    try:
+        # Insert into user_flags table the username and flag they have obtained
+        c.execute('''INSERT INTO user_points VALUES (?,?)''', (username, '0'))
+    except Exception, e:
+        logger.inf(e)
+
+    try:
+        conn.commit()    # commit the changes to the database
+    except Exception, e:
+        logger.inf(e)
+
+    try:
+        conn.close()    # Close connection to sqlite database
+    except Exception, e:
+        logger.inf(e)
