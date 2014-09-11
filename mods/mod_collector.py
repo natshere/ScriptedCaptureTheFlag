631cd063bb518aada1baf35b9d7c942adbb02056
tgorup <tom@rookconsulting.com>
2014-09-10 21:16:22 -0400

    Increased logging and exception handling.

--- mods/mod_collector.py
+++ mods/mod_collector.py
@@ -50,16 +50,29 @@ def check_if_uuid_exists(uuid):    # Check if flag exists
 def check_if_user_exists(username):
 
     import sqlite3
-    conn = sqlite3.connect('database/ctfCollector.db')    # Setup connection to sqlite database
-    c = conn.cursor()
 
-    c.execute('''SELECT EXISTS(SELECT * FROM users WHERE uname = ?)''', (username,))    # Check if user exists
-    returnvalue = c.fetchone()
+    try:
+        conn = sqlite3.connect('database/ctfCollector.db')    # Setup connection to sqlite database
+        c = conn.cursor()
+    except Exception, e:
+        logger.info(e)
+
+    try:
+        c.execute('''SELECT EXISTS(SELECT * FROM users WHERE uname = ?)''', (username,))    # Check if user exists
+    except Exception, e:
+        logger.info(e)
+
+    try:
+        returnvalue = c.fetchone()
+    except Exception, e:
+        logger.info(e)
+
     return returnvalue[0]
 
 def check_if_user_exists_points(username):
 
     import sqlite3
+
     try:
         conn = sqlite3.connect('database/ctfCollector.db')    # Setup connection to sqlite database
         c = conn.cursor()
@@ -68,6 +81,10 @@ def check_if_user_exists_points(username):
 
     try:
         c.execute('''SELECT EXISTS(SELECT * FROM user_points WHERE uname = ?)''', (username,))    # Check if user exists
+    except Exception, e:
+        logger.info(e)
+
+    try:
         returnvalue = c.fetchone()
     except Exception, e:
         logger.info(e)
@@ -76,10 +93,14 @@ def check_if_user_exists_points(username):
 
 def checkdatabase(database):    # Validate the database exists in the correct directory. Recommend running setup.py otherwise
 
-    import os
-    if not os.path.isfile(os.path.realpath('database/' + database)):    # Check if database file in database/ does not exist
-        print('Run setup.py first')    # Print next steps if not
-        exit()    # Exit script if database doesn't exist
+    try:
+        if not os.path.isfile(os.path.realpath('database/' + database)):    # Check if database file in database/ does not exist
+            logger.info('Database does not exist. Run setup first')
+            print('Database does not exist. Run setup.py first')    # Print next steps if not
+            exit()    # Exit script if database doesn't exist
+    except Exception, e:
+        logger.info(e)
+
 
 def decrypt_RSA(private_key_loc, package):
 
@@ -93,63 +114,160 @@ def decrypt_RSA(private_key_loc, package):
     from Crypto.Cipher import PKCS1_OAEP
     from base64 import b64decode
 
-    key = open(private_key_loc, "r").read()
+    try:
+        key = open(private_key_loc, "r").read()
+    except Exception, e:
+        logger.info(e)
 
-    rsakey = RSA.importKey(key)
-    rsakey = PKCS1_OAEP.new(rsakey)
+    try:
+        rsakey = RSA.importKey(key)
+    except Exception, e:
+        logger.info(e)
 
-    decrypted = rsakey.decrypt(b64decode(package))
+    try:
+        rsakey = PKCS1_OAEP.new(rsakey)
+    except Exception, e:
+        logger.info(e)
+
+    try:
+        decrypted = rsakey.decrypt(b64decode(package))
+    except Exception, e:
+        logger.info(e)
+
     return decrypted
 
 def sanitize(table_name):    # On standby for the need to scrub sql input
 
-    return ''.join( chr for chr in table_name if chr.isalnum() )    # Return string with only allow alphanumeric characters
+    try:
+        clean_table_name = ''.join( chr for chr in table_name if chr.isalnum() )    # Return string with only allow alphanumeric characters
+    except Exception, e:
+        logger.info(e)
+
+    return clean_table_name
 
 def update_user_flag(username, flag):    # Add flag to the user - used to allow only one time use of flags
 
     import sqlite3
-    import logging
 
-    conn = sqlite3.connect('database/ctfCollector.db')    # Setup connection to sqlite database
-    logging.info("Connecting to ctfCollector.db setup")    # Log to informational
-    c = conn.cursor()
-    # Insert into user_flags table the username and flag they have obtained
-    c.execute('''INSERT INTO user_flags VALUES (?,?)''', (username, flag))
-    conn.commit()    # commit the changes to the database
-    conn.close()    # Close connection to sqlite database
-    logging.info("Closing connection to database")    # Log to info
+    try:
+        conn = sqlite3.connect('database/ctfCollector.db')    # Setup connection to sqlite database
+        logging.info("Connecting to ctfCollector.db setup")    # Log to informational
+    except Exception, e:
+        logger.info(e)
+
+    try:
+        c = conn.cursor()
+    except Exception, e:
+        logger.info(e)
+
+    try:
+        # Insert into user_flags table the username and flag they have obtained
+        c.execute('''INSERT INTO user_flags VALUES (?,?)''', (username, flag))
+    except Exception, e:
+        logger.info(e)
+
+    try:
+        conn.commit()    # commit the changes to the database
+    except Exception, e:
+        logger.info(e)
+
+    try:
+        conn.close()    # Close connection to sqlite database
+        logging.info("Closing connection to database")    # Log to info
+    except Exception, e:
+        logger.info(e)
 
 def get_users_current_score(username):
 
     import sqlite3
-    conn = sqlite3.connect('database/ctfCollector.db')    # Setup connection to sqlite database
-    c = conn.cursor()
 
-    c.execute('''SELECT tot_points FROM user_points WHERE uname = ?''', (username,))    # Check if user exists
-    returnvalue = c.fetchone()
-    c.close()
+    try:
+        conn = sqlite3.connect('database/ctfCollector.db')    # Setup connection to sqlite database
+    except Exception, e:
+        logger.info(e)
+
+    try:
+        c = conn.cursor()
+    except Exception, e:
+        logger.info(e)
+
+    try:
+        c.execute('''SELECT tot_points FROM user_points WHERE uname = ?''', (username,))    # Check if user exists
+    except Exception, e:
+        logger.info(e)
+
+    try:
+        returnvalue = c.fetchone()
+    except Exception, e:
+        logger.info(e)
+
+    try:
+        c.close()
+    except Exception, e:
+        logger.info(e)
+
     return returnvalue[0]
 
 def get_flag_worth(uuid):
 
     import sqlite3
-    conn = sqlite3.connect('database/ctfCollector.db')    # Setup connection to sqlite database
-    c = conn.cursor()
 
-    c.execute('''SELECT points FROM flags WHERE uuid = ?''', (uuid,))    # Check if user exists
-    returnvalue = c.fetchone()
-    c.close()
+    try:
+        conn = sqlite3.connect('database/ctfCollector.db')    # Setup connection to sqlite database
+    except Exception, e:
+        logger.info(e)
+
+    try:
+        c = conn.cursor()
+    except Exception, e:
+        logger.info(e)
+
+    try:
+        c.execute('''SELECT points FROM flags WHERE uuid = ?''', (uuid,))    # Check if user exists
+    except Exception, e:
+        logger.info(e)
+
+    try:
+        returnvalue = c.fetchone()
+    except Exception, e:
+        logger.info(e)
+
+    try:
+        c.close()
+    except Exception, e:
+        logger.info(e)
+
     return returnvalue[0]
 
 def get_is_flag_venomous(uuid):
 
     import sqlite3
-    conn = sqlite3.connect('database/ctfCollector.db')    # Setup connection to sqlite database
-    c = conn.cursor()
 
-    c.execute('''SELECT venomous FROM flags WHERE uuid = ?''', (uuid,))    # Check if user exists
-    returnvalue = c.fetchone()
-    c.close()
+    try:
+        conn = sqlite3.connect('database/ctfCollector.db')    # Setup connection to sqlite database
+    except Exception, e:
+        logger.info(e)
+
+    try:
+        c = conn.cursor()
+    except Exception, e:
+        logger.info(e)
+
+    try:
+        c.execute('''SELECT venomous FROM flags WHERE uuid = ?''', (uuid,))    # Check if user exists
+    except Exception, e:
+        logger.info(e)
+
+    try:
+        returnvalue = c.fetchone()
+    except Exception, e:
+        logger.info(e)
+
+    try:
+        c.close()
+    except Exception, e:
+        logger.info(e)
+
     return returnvalue[0]
 
 def update_user_score(username, uuid):    # Update users score
@@ -157,30 +275,91 @@ def update_user_score(username, uuid):    # Update users score
     import sqlite3
     import mod_global as global_def
 
-    if not check_if_user_exists_points(username):
-        global_def.insert_new_user(username)
+    try:
+        if not check_if_user_exists_points(username):
+            try:
+                global_def.insert_new_user(username)
+            except Exception, e:
+                logger.info(e)
+    except Exception, e:
+        logger.info(e)
 
-    points = get_users_current_score(username) # Get users current score for adding and updating
-    flag = get_flag_worth(uuid)
+    try:
+        points = get_users_current_score(username) # Get users current score for adding and updating
+    except Exception, e:
+        logger.info(e)
 
-    if not get_is_flag_venomous(uuid):    # Check if flag is not venomous
-        new_points = int(points) + int(flag)    # Add if flag is not venomous
-    else:
-        new_points = int(points) - int(flag)    # Subtract if flag is venomous
+    try:
+        flag = get_flag_worth(uuid)
+    except Exception, e:
+        logger.info(e)
 
-    conn = sqlite3.connect('database/ctfCollector.db')    # Setup connection to sqlite database
-    c = conn.cursor()
-    # Insert into user_flags table the username and flag they have obtained
-    c.execute('''UPDATE user_points SET tot_points=? WHERE uname=?''', (new_points, username))
-    conn.commit()    # commit the changes to the database
-    conn.close()    # Close connection to sqlite database
+    try:
+        if not get_is_flag_venomous(uuid):    # Check if flag is not venomous
+            try:
+                new_points = int(points) + int(flag)    # Add if flag is not venomous
+            except Exception, e:
+                logger.info(e)
+        else:
+            try:
+                new_points = int(points) - int(flag)    # Subtract if flag is venomous
+            except Exception, e:
+                logger.info(e)
+    except Exception, e:
+        logger.info(e)
+
+    try:
+        conn = sqlite3.connect('database/ctfCollector.db')    # Setup connection to sqlite database
+    except Exception, e:
+        logger.info(e)
+
+    try:
+        c = conn.cursor()
+    except Exception, e:
+        logger.info(e)
+
+    try:
+        # Insert into user_flags table the username and flag they have obtained
+        c.execute('''UPDATE user_points SET tot_points=? WHERE uname=?''', (new_points, username))
+    except Exception, e:
+        logger.info(e)
+
+    try:
+        conn.commit()    # commit the changes to the database
+    except Exception, e:
+        logger.info(e)
+
+    try:
+        conn.close()    # Close connection to sqlite database
+    except Exception, e:
+        logger.info(e)
 
 def user_message_update(username, message):    # Update user messages table
 
     import sqlite3
-    conn = sqlite3.connect('database/ctfCollector.db')    # Setup connection to sqlite database
-    c = conn.cursor()
-    # Insert into user_flags table the username and flag they have obtained
-    c.execute('''INSERT INTO user_messages VALUES (?,?)''', (username, message))
-    conn.commit()    # commit the changes to the database
-    conn.close()    # Close connection to sqlite database
+
+    try:
+        conn = sqlite3.connect('database/ctfCollector.db')    # Setup connection to sqlite database
+    except Exception, e:
+        logger.info(e)
+
+    try:
+        c = conn.cursor()
+    except Exception, e:
+        logger.info(e)
+
+    try:
+        # Insert into user_flags table the username and flag they have obtained
+        c.execute('''INSERT INTO user_messages VALUES (?,?)''', (username, message))
+    except Exception, e:
+        logger.info(e)
+
+    try:
+        conn.commit()    # commit the changes to the database
+    except Exception, e:
+        logger.info(e)
+
+    try:
+        conn.close()    # Close connection to sqlite database
+    except Exception, e:
+        logger.info(e)
