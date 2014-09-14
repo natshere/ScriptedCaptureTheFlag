0385030e65e5304d39eb20ca02f30b448d153321
tgorup <tom@rookconsulting.com>
2014-09-13 23:38:18 -0400

    More descriptive logging.

--- setup.py
+++ setup.py
@@ -1,13 +1,8 @@
 #!/usr/bin/python
 
-import argparse
-import mods.mod_global as global_def
 import mods.mod_setup as setup_def
 import logging
 import os
-
-parser = argparse.ArgumentParser(description='Server listening for flags')
-parser.add_argument('-l', '--loglevel', help='Logging level - followed by debug, info, or warning')
 
 current_directory = os.getcwd()
 logger = logging.getLogger('ctfCollector')
@@ -20,46 +15,36 @@ logger.setLevel(logging.INFO)
 if __name__ == "__main__":
 
     try:
-        args = vars(parser.parse_args())    # Assign arguments to args array
-    except Exception, e:
-        logger.info(e)
-
-    try:
-        global_def.loglevel(args['loglevel'])    # Set log levels
-    except Exception, e:
-        logger.info(e)
-
-    try:
         setup_def.checkModules()    # Validate modules exist
     except Exception, e:
-        logger.info(e)
+        logger.info("Call to setup_def.checkModules: {0}".format(e))
 
     try:
         setup_def.setupDatabase('ctfCollector.db')    # Setup the database
     except Exception, e:
-        logger.info(e)
+        logger.info("Call to setup_def.setupDatabase: {0}".format(e))
 
     try:
         encryption = setup_def.generate_RSA()    # Generate public and private keys
     except Exception, e:
-        logger.info(e)
+        logger.info("Call to setup_def.generate_RSA: {0}".format(e))
 
     try:
         f = open('keys/priv.key', 'w')    # Write private key
     except Exception, e:
-        logger.info(e)
+        logger.info("Open priv.key for writing: {0}".format(e))
 
     try:
         f.write(encryption[0])
     except Exception, e:
-        logger.info(e)
+        logger.info("Write the private key: {0}".format(e))
 
     try:
         f = open('keys/pub.key', 'w')    # Write public key
     except Exception, e:
-        logger.info(e)
+        logger.info("Open pub.key for writing: {0}".format(e))
 
     try:
         f.write(encryption[1])
     except Exception, e:
-        logger.info(e)
+        logger.info("Write the public key: {0}".format(e))
