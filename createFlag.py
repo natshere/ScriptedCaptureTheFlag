6c62aae4322bbf4ffe2ad20b83ec863cda1b21ac
tgorup <tom@rookconsulting.com>
2014-09-13 23:23:32 -0400

    More descriptive logging. Changed log file name

--- createFlag.py
+++ createFlag.py
@@ -17,7 +17,7 @@ parser.add_argument('-u', '--justuuid', help='Enter to create just a uuid and no
 
 current_directory = os.getcwd()
 logger = logging.getLogger('ctfCollector')
-hdlr = logging.FileHandler(current_directory + '/log/creatFlag.log')
+hdlr = logging.FileHandler(current_directory + '/log/createFlag.log')
 formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
 hdlr.setFormatter(formatter)
 logger.addHandler(hdlr)
@@ -27,7 +27,7 @@ if __name__ == "__main__":
     try:
         args = vars(parser.parse_args())    # Assign arguments to args variable
     except Exception, e:
-        logger.info(e)
+        logger.info("Call to args parser: {0}".format(e))
 
     try:
         flagUUID = uuid.uuid4()    # Create new uuid and assign to variable
@@ -37,27 +37,27 @@ if __name__ == "__main__":
     try:
         flagname = args['name']
     except Exception, e:
-        logger.info(e)
+        logger.info("Get flag name from args: {0}".format(e))
 
     try:
         ipaddress = args['ipaddress']
     except Exception, e:
-        logger.info(e)
+        logger.info("Get ipaddress from args: {0}".format(e))
 
     try:
         if create_flag_def.check_if_flagname_exists(flagname):    # Check if flag name already exists, ask user for new one if does
             print "Flag name already exists, please select a new one: ",
             flagname = raw_input()
     except Exception, e:
-        logger.info(e)
+        logger.info("Get flagname from args: {0}".format(e))
     try:
         if create_flag_def.check_if_uuid_exists(flagUUID):
             try:
                 flagUUID = uuid.uuid4()    # Create new uuid and assign to variable
             except Exception, e:
-                logger.info(e)
+                logger.info("Get randomized UUID: {0}".format(e))
     except Exception, e:
-        logger.info(e)
+        logger.info("Call to create_flag_def.check_if_uuid_exists: {0}".format(e))
 
     try:
         if not args['justuuid']:
@@ -65,25 +65,25 @@ if __name__ == "__main__":
             try:
                 pubKey = open(public_key_loc, "r").read()    # Feed the key to variable for writing
             except Exception, e:
-                logger.info(e)
+                logger.info("Open public_key_loc: {0}".format(e))
 
             try:
                 create_flag_def.createFlag(flagname, pubKey, flagUUID, ipaddress)    # Create the new flag
             except Exception, e:
-                logger.info(e)
+                logger.info("Call to create_flag_def.createFlag: {0}".format(e))
 
             try:
                 create_flag_def.update_uuid_db(flagname, str(flagUUID), int(args['points']), args['venomous'])    # Update the database with the information
             except Exception, e:
-                logger.info(e)
+                logger.info("Call to create_flag_def.update_uuid_db: {0}".format(e))
         else:
             try:
                 print "New Flag UUID: " + str(flagUUID)
             except Exception, e:
-                logger.info(e)
+                logger.info("Printing new flag as string: {0}".format(e))
             try:
                 create_flag_def.update_uuid_db(flagname, str(flagUUID), int(args['points']), args['venomous'])    # Update the database with the information
             except Exception, e:
-                logger.info(e)
+                logger.info("Call to create_flag_def.update_uuid_db: {0}".format(e))
     except Exception, e:
         logger.info(e)
