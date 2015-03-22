07b5c6b280557faaf4c65ef8204dfefc6647c8fc
nshere <nat.shere@rooksecurity.com>
2015-03-22 18:53:21 -0400

    First server push
    Added a scores.html file and incorporated server side logic for
    populating the scores table from the database.
    TO-DO: run it locally and test
    TO-DO: Admin log in page and configuration page
    TO-DO: stylize the scores.html page better

---
+++ server.py
@@ -0,0 +1,137 @@
+#!/usr/bin/python
+
+from BaseHTTPServer import BaseHTTPRequestHandler,HTTPServer
+from os import curdir, sep
+import ssl
+import cgi
+import sqlite3
+import hashlib
+import time, datetime
+import Cookie
+import uuid
+import logging
+
+PORT_NUMBER = 7665
+
+# Handler for incoming web requests
+class myHandler(BaseHTTPRequestHandler):
+	current_directory = os.getcwd()
+	logger = logging.getLogger('ctfCollector')
+	hdlr = logging.FileHandler(current_directory + '/log/ctfCollector.log')
+	formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
+	hdlr.setFormatter(formatter)
+	logger.addHandler(hdlr)
+	logger.setLevel(logging.INFO)
+
+	def getCurrentScores(self):
+		try:
+	        conn = sqlite3.connect('database/ctfCollector.db')    # Setup connection to sqlite database
+	    except Exception, e:
+	        logger.info("Setup connection to database: {0}".format(e))
+
+	    try:
+	        c = conn.cursor()
+	    except Exception, e:
+	        logger.info("Setup cursor: {0}".format(e))
+
+	    try:
+	        # Insert into user_flags table the username and flag they have obtained
+	        c.execute('''SELECT * FROM user_points''')
+	        points = c.fetchall()
+	        if len(points) > 0:
+	        	scores = '<h1>Top Scores</h1><table><tr><th>User</th><th>Score</th></tr>'
+	        	for p in points:
+	        		scores += '<tr><td>' + str(p[0]) + '</td><td>' + str(p[1]) + '</td></tr>'
+	        	scores += '</table>'
+	        	return scores
+	        else:
+	        	return '<p>No scores recorded yet....</p>'
+	    except Exception, e:
+	        logger.info("UPDATE user_points: {0}".format(e))
+
+
+	def do_GET(self):
+		if not self.path == 'background.png' and not self.path == 'updateScores' and not self.path == 'login.html':
+			self.path = 'serverFiles/scores.html'
+			mimetype='text/html'
+			f = open(curdir + sep + self.path) 
+			self.send_response(200)
+			self.send_header('Content-type',mimetype)
+			self.end_headers()
+			contents = f.read()
+			contents = contents.replace('&&&', self.getCurrentScores())
+			self.wfile.write(contents)
+			f.close()
+			return
+
+		elif self.path == 'updateScores':
+			return self.getCurrentScores()
+		
+		elif self.path == 'login.html':
+			self.path = 'serverFiles/login.html'
+			mimetype = 'text/html'
+			f = open(curdir + sep + self.path) 
+			self.send_response(200)
+			self.send_header('Content-type',mimetype)
+			self.end_headers()
+			self.wfile.write(f.read())
+			f.close()
+		# Available js, css, and images are hard-coded paths
+		else:
+			self.path = 'serverFiles/background.png'
+			mimetype = 'image/png'
+			f = open(curdir + sep + self.path) 
+			self.send_response(200)
+			self.send_header('Content-type',mimetype)
+			self.end_headers()
+			self.wfile.write(f.read())
+			f.close()
+			return
+
+
+	def do_POST(self):
+		if self.path=='/login':
+			form = cgi.FieldStorage(
+				fp=self.rfile, 
+				headers=self.headers,
+				environ={'REQUEST_METHOD':'POST',
+		                 'CONTENT_TYPE':self.headers['Content-Type'],
+			})
+			# perform login authentication and authorization
+			return
+
+		elif self.path == '/admin':
+			form = cgi.FieldStorage(
+				fp=self.rfile, 
+				headers=self.headers,
+				environ={'REQUEST_METHOD':'POST',
+		                 'CONTENT_TYPE':self.headers['Content-Type'],
+			})
+			# process admin request
+			return
+
+		else:
+			self.path = 'serverFiles/scores.html'
+			mimetype='text/html'
+			f = open(curdir + sep + self.path) 
+			self.send_response(200)
+			self.send_header('Content-type',mimetype)
+			self.end_headers()
+			contents = f.read()
+			contents = contents.replace('&&&', self.getCurrentScores())
+			self.wfile.write(contents)
+			f.close()
+			return
+
+			
+try:
+	#Create a web server and define the handler to manage the
+	#incoming request
+	httpd = HTTPServer(('', 7665), myHandler)
+	httpd.socket = ssl.wrap_socket(httpd.socket, certfile='./certs/server.pem', server_side=True)
+	print 'Serving HTTPS on port 7665'
+	httpd.serve_forever()
+
+except KeyboardInterrupt:
+	print '^C received, shutting down the web server'
+	httpd.socket.close()
